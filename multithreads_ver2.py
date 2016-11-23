2637e050b605669068b5f264e184b2be19f661df
Juuso Sipola <juuso_sipola@hotmail.com>
2016-11-23 14:15:02 +0200

    newest ver

--- multithreads_ver2.py
+++ multithreads_ver2.py
@@ -23,41 +23,55 @@ class serialLink(threading.Thread):
             #print("add buffer")
             quit = 0
             ctr = 0
-            tempA = 0
+            tempB = 0
+            tempC = 0
             defA, defB, defC = None, None, None
             s = threading.currentThread()
             while getattr(s,"do_run", True): # waits for the event for the blender window
                 try:
                     line = self._ser.readline()
                     line = line.decode('utf-8')
-                    line = line.strip('\r\n')
-                    line = line.split('\t')
-                    print(line)
-                    if len(line) == 4:
-                        qlock.acquire()
-                        a = float(line[1])
-                        b = float(line[2])
-                        c = float(line[3])
-                        if ctr < 5:
-                            print("aasi")
-                            if (a == tempA):
-                                ctr += 1
-                                print(ctr)
-                            else:
-                                #print("else ctr")
-                                ctr = 0
-                            tempA = a
-                            if ctr == 5:
-                                defA, defB, defC = float(a), float(b), float(c)
-                                ctr = 6
-                                print("YAW alustettu")
-                        if defA:
-                            if a > defA + 30 or a < defA - 30 or b > defB + 30 or b < defB - 30 or c > defC + 30 or c < defC - 30:
-                                #print(line)
-                                q.put(line)
-                    #print(line)
-                    #q.put(line)
-                        qlock.release()
+                    if not line.find("demo") == -1:
+                        self._ser.write(str.encode("a"))
+                        defA = None
+                        ctr = 0
+                        continue
+                    else:
+                        line = line.strip('\r\n')
+                        line = line.split('\t')
+                        print(line)
+                        if len(line) == 4:
+                            qlock.acquire()
+                            a = float(line[1])
+                            b = float(line[2])
+                            c = float(line[3])
+                            
+                            if ctr < 5:
+                                print("aasi")
+                                if (c == tempC and b == tempB):
+                                    ctr += 1
+                                    print(ctr)
+                                else:
+                                    #print("else ctr")
+                                    ctr = 0
+                                tempC = c
+                                tempB = b
+                                if ctr == 5:
+                                    defA, defB, defC = float(a), float(b), float(c)
+                                    ctr = 6
+                                    print("YAW alustettu")
+                            
+                            if defA:
+                                if a > defA + 30 or a < defA - 30 or b > defB + 30 or b < defB - 30 or c > defC + 30 or c < defC - 30:
+                                    #print(line)
+                                    line.append(defB)
+                                    line.append(defC)
+                                    q.put(line)
+                            
+                            #q.put(line)
+                        #print(line)
+                        #q.put(line)
+                            qlock.release()
                 except TypeError:
                     print("type error")
                 except UnicodeError:
@@ -69,7 +83,7 @@ class serialLink(threading.Thread):
                 except KeyboardInterrupt:
                     print("KeyboardInterrupt!!!!")
                     
-                    #ser.close()
+                    self._ser.close()
                     quit = 1
                 #finally:
                 #    ser.close()
@@ -92,7 +106,11 @@ class serialLink(threading.Thread):
         print("done calibrating")
 
     def checkPorts(self):
-        a = list(serial.tools.list_ports.comports())
+        a = []
+        while len(a) == 0:
+            time.sleep(5)
+            a = list(serial.tools.list_ports.comports())
+        #print(a)
         if sys.platform.startswith('win'):
             for port in a:
                 if "Arduino" in port[1]:
@@ -151,6 +169,8 @@ class ModalTimerOperator(bpy.types.Operator):
             a = float(line[1])
             b = float(line[2])
             c = float(line[3])
+            defB = float(line[4])
+            defC = float(line[5])
             pi = 3.14159265358979
             qlock.release()
             #obj.rotation_euler = (b , c , a)
@@ -158,30 +178,30 @@ class ModalTimerOperator(bpy.types.Operator):
             
             if a > obj.rotation_euler.z + 30:
      #           rotation.append("+a")
-                obj.rotation_euler.z -= pi/16
-                #pass
+                #obj.rotation_euler.z -= pi/16
+                pass
                            
             if a < obj.rotation_euler.z - 30:
     #            rotation.append("-a")       
-                obj.rotation_euler.z += pi/16
-                #pass
+                #obj.rotation_euler.z += pi/16
+                pass
                     
-            if b > obj.rotation_euler.x + 30:
+            if c > defC + 30: # c < obj.rotation_euler.y + 30
                 #rotation.append("+b") 
-                obj.rotation_euler.x += pi/16
+                obj.rotation_euler.y += pi/16
                 #pass     
-            if b < obj.rotation_euler.x - 30:
+            if c < defC - 30: # c > obj.rotation_euler.y
                 #rotation.append("-b")
-                obj.rotation_euler.x -= pi/16
+                obj.rotation_euler.y -= pi/16
                 #pass
                     
-            if c > obj.rotation_euler.y + 30:
+            if b > defB + 30: # b > obj.rotation_euler.x
                 #rotation.append("+c") 
-                obj.rotation_euler.y += pi/16     
+                obj.rotation_euler.x += pi/16     
                 #pass
-            if c < obj.rotation_euler.y - 30:
+            if b < defB - 30: # b > obj.rotation_euler.x
                 #rotation.append("-c")
-                obj.rotation_euler.y -= pi/16
+                obj.rotation_euler.x -= pi/16
                 #pass
             
             #time.sleep(0.1)
@@ -220,6 +240,21 @@ class ModalTimerOperator(bpy.types.Operator):
         
 
 
+def rotateCamera():
+    #bpy.ops.object.delete(use_global=False)
+    #bpy.ops.mesh.primitive_cube_add()
+    for area in bpy.context.screen.areas:
+        if area.type == 'VIEW_3D':
+            override = bpy.context.copy()
+            override['area'] = area
+            bpy.ops.view3d.viewnumpad(override, type = 'FRONT')
+            #bpy.ops.view3d.view_orbit(type = 'ORBITUP')
+            bpy.ops.object.mode_set(mode = 'EDIT')
+            #bpy.ops.mesh.subdivide(number_cuts = 20)
+            bpy.ops.object.mode_set(mode='SCULPT')
+            break
+
+
 def register():
     bpy.utils.register_class(ModalTimerOperator)
 
@@ -238,5 +273,6 @@ if __name__ == "__main__":
     p.start()
     #p.join()
     print("avattu")
+    rotateCamera()
     # test call
     bpy.ops.wm.modal_timer_operator()
