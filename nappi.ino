abbdad4f9b06ecaec5ca895d2e47bee1eb8803be
Juuso Sipola <juuso_sipola@hotmail.com>
2016-11-24 14:35:20 +0200

    Button test

---
+++ nappi.ino
@@ -0,0 +1,14 @@
+int pin3 = 2;
+int state = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+ pinMode(pin3,INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  state = digitalRead(pin3);
+  Serial.println(state);
+  delay(500);
+}
